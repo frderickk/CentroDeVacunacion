709ee47912ba05554cdf0ea8551fd176f7372e66
frderickk <federicofarias@outlook.com>
2021-06-03 17:56:06 -0300

    Se agrega clase Heladera. Version ultra Alpha

---
+++ src/Heladera.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Heladera {
+	
+	private HashMap<String, ArrayList<Vacunas>> vacunas;
+	private int vacunasDisponibles;
+	
+	public Heladera() {
+		
+		this.vacunasDisponibles = 0;
+		
+		vacunas = new HashMap<String, ArrayList<Vacunas>>();
+		
+	}
+	
+
+}
